9d3a288aceded0cc7c89461edbfbd3b6b67a1842
mcc1461 <mcoskuncelebi@gmail.com>
2024-06-12 21:36:14 +0300

    fix: tailwindcss config problem solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import './App.css'
-
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>React-Router-Dom</h1>
+      <h1 className='bg-green-300'>React router dom</h1>
     </>
   )
 }
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import {Outlet} from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 
-
-const Layout = () => {
+function Layout() {
   return (
     <>
-        <Header />
-        <Outlet />
-        <Footer />  
+    <Header />
+    <Outlet />
+    <Footer />
     </>
   )
 }
